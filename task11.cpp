4a1ca091f1aae9c7cfe98471c4da3c5a34631e2e
pro-GRMR <73429648+pro-GRMR@users.noreply.github.com>
2020-10-30 13:55:38 +0600

    Create task11.cpp

---
+++ task11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, sum=0;
+    cin >> x;
+    for(int i=1; i<x; i++){
+        if(i%3==0 || i%5==0){
+            sum +=i;
+        }
+    }
+    cout << sum << endl;
+}
